459a87826bf3d291f5485aa7dd9cc7ed04c8f73b
Smoke0Dev <cchullo@unsa.edu.pe>
2026-06-01 14:42:54 -0500

    Primer commit: Actividad 1

---
+++ solu.c
@@ -0,0 +1,41 @@
+#include "../turtlec.h"
+
+void fractalTree(Turtle *turtle, int length, int depth){
+     if(depth == 0 || length < 5)
+         return;
+     
+     turtleForward(turtle, length);
+     
+     turtleLeft(turtle, 30);
+     fractalTree(turtle, length * 0.7, depth - 1);
+
+     turtleRight(turtle, 60);
+     fractalTree(turtle, length * 0.7, depth - 1);
+
+     turtleLeft(turtle, 30);
+     turtleBackward(turtle, length);
+}
+
+int main(void){
+  TurtleApp *app = turtleAppCreate(800, 600, "Test Line");
+
+  if(app == NULL)
+    return 1;
+
+  Turtle *t = turtleAppGetTurtle(app);
+
+  turtleSetColor(t, 255, 100, 0);
+  turtleSetSpeed(t, 5.0f);
+
+  turtlePenUp(t);
+  turtleGoTo(t, 400, 550);
+  turtlePenDown(t);
+  
+  turtleLeft(t, 90);
+  fractalTree(t, 100, 5);
+  
+  turtleAppRun(app);
+  turtleAppDestroy(app);
+  return 0;
+}
+
